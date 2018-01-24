bf0e83e89acac65d811e6900e20f5617a6ece2e6
MarcelaV <m.vilchespino@gmail.com>
2018-01-24 10:32:03 -0300

    Version con like y dislike funcional. Hasta video 18 de contenido 2

--- src/picture-card/index.js
+++ src/picture-card/index.js
@@ -1,21 +1,45 @@
 var yo = require('yo-yo');
 
 
-module.exports = function (pic) {
-	return yo`<div class="card">
-    	<div class="card-image">
-      		<img class="activator" src="${pic.url}">
-    	</div>
-    	<div class="card-content">
-      		<a href="/user/${pic.user.username}" class="card-title">
-				<img src="${pic.user.avatar}" class="avatar">
-				<span class="username">${pic.user.username}</span>
-      		</a>
-			<small class="right time">Hace un dia</small>
-			<p>
-				<a class="left" href="#"><i class="fa fa-heart-o" aria-hidden="true"></i></a>
-				<span class="left likes">${pic.likes} Me gusta</span>
-			</p>
-    	</div>
-  	</div>`;
+module.exports = function pictureCard(pic) {
+	var el;
+
+	function render(picture) {
+		return yo`<div class="card ${picture.liked ? 'liked' : ''}">
+	    	<div class="card-image">
+	      		<img class="activator" src="${pic.url}">
+	    	</div>
+	    	<div class="card-content">
+	      		<a href="/user/${pic.user.username}" class="card-title">
+					<img src="${pic.user.avatar}" class="avatar">
+					<span class="username">${pic.user.username}</span>
+	      		</a>
+				<small class="right time">Hace un dia</small>
+				<p>
+					<a class="left" href="#" onclick=${like}><i class="fa fa-heart-o" aria-hidden="true"></i></a>
+					<a class="left" href="#" onclick=${dislike}><i class="fa fa-heart" aria-hidden="true"></i></a>
+					<span class="left likes">${pic.likes} Me gusta</span>
+				</p>
+	    	</div>
+	  	</div>`;
+	}
+
+  	function like(){
+  		pic.liked = true;
+  		pic.likes++;
+  		var newEl = render(pic);
+  		yo.update(el, newEl);
+  		return false;
+  	}
+
+  	function dislike(){
+  		pic.liked = false;
+  		pic.likes--;
+  		var newEl = render(pic);
+  		yo.update(el, newEl);
+  		return false;
+  	}
+
+  	el = render(pic);
+  	return el;
 }
